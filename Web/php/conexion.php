5be93273ec0e9cffb6157e36222e457d6d76ee57
Nahia Beraza <nahia.beraza06@somo.eus>
2026-02-07 10:47:58 +0100

    Cambio de conexion

--- Web/php/conexion.php
+++ Web/php/conexion.php
@@ -1,15 +1,14 @@
 <?php
-    function conexionBD() {
-        $host = "localhost";
-        $usuario = "root";
-        $contrasena = "admin";
-        $baseDeDatos = "red_social_db";
+function conexionBD() {
+    $host = "localhost";
+    $usuario = "laredsocial";
+    $contrasena = "";   // SIN contraseña
+    $baseDeDatos = "red_social_db";
 
-        $conexion = mysqli_connect($host, $usuario, $contrasena, $baseDeDatos) or die("Problemas con la conexión");
+    $conexion = mysqli_connect($host, $usuario, $contrasena, $baseDeDatos)
+        or die("Problemas con la conexión");
 
-        //Esta línea es clave para que los acentos y la ñ se vean bien
-        $conexion->set_charset("utf8");
-        return $conexion;
-
-    }
+    $conexion->set_charset("utf8");
+    return $conexion;
+}
 ?>
